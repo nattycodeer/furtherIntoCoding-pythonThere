da3791f01bd58333a9008e1ce172b975d6d2e541
nattycodeer <code@shalomkeshet.com>
2020-11-29 21:02:32 +0530

    Made changes in get_all_items.py.

--- python_three/dictionary/get_all_items.py
+++ python_three/dictionary/get_all_items.py
@@ -1,2 +1,5 @@
 pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
-print(pct_women_in_occupation)
+
+for designation, percent in pct_women_in_occupation.items():
+  print("Women make up " + str(percent) + " percent of " + designation + "s")
+
