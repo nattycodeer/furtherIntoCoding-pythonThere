eec08e6d5e1df7310c5fc7f0eb44be6b1dd075f7
Shalom Keshet <75151273+nattycodeer@users.noreply.github.com>
2020-11-30 12:45:42 +0530

    Updated get_all_items.py

--- python_three/dictionary/get_all_items.py
+++ python_three/dictionary/get_all_items.py
@@ -1,5 +1,4 @@
 pct_women_in_occupation = {"CEO": 28, "Engineering Manager": 9, "Pharmacist": 58, "Physician": 40, "Lawyer": 37, "Aerospace Engineer": 9}
-
 for designation, percent in pct_women_in_occupation.items():
-  print("Women make up " + str(percent) + " percent of " + designation + "s")
-
+  print("Women make up {} percent of {}s".format(percent, designation))
+  # print(f"Women make up {percent} percent of {designation}s")
